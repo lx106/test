0f69d5ae122ea797403a6a80632267fdf35ddba0
lx106 <1062461248@qq.com>
2017-07-17 22:20:33 +0800

    Delete HelloWorld.java
    第一次删除文件

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-
-	public static void main(String[] args) {
-		
-		System.out.println(Test.class.getClassLoader());
-		System.out.println(Test.class.getClassLoader());
-		System.out.println(Test.class.getClassLoader());
-	}
-}
