67db151b6dc040fdc44ee20c016cab31c6a8df8e
lx106 <1062461248@qq.com>
2017-07-17 22:10:55 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.println(Test.class.getClassLoader());
+		System.out.println(Test.class.getClassLoader());
 		System.out.println(Test.class.getClassLoader());
 	}
 }
